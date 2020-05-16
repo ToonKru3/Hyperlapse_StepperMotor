f5e3519c9929cc174b1adf5e590ff27a2528dbd7
ToonKru3 <56877894+ToonKru3@users.noreply.github.com>
2020-05-16 13:51:15 +0700

    Arduino Coding Version 1.0

---
+++ StepperMotor/StepperMotor.ino
@@ -0,0 +1,22 @@
+#define dirPin 6
+#define stepPin 7
+#define stepsPerRevolution 200
+
+void setup() {
+  // Declare pins as output:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+void loop() {
+
+  digitalWrite(dirPin, HIGH);
+  // Spin the stepper motor 1 revolution slowly:
+  for (int i = 0; i < stepsPerRevolution; i++) 
+  {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW); 
+    delayMicroseconds(1000);
+  }
+}
